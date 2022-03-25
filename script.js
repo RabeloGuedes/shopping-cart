6536fc2b8ebdffd00f65dea41bfd72450355f02b
RabeloGuedes <krepscredian@gmail.com>
2022-03-25 20:33:31 -0300

    feat: Calcula o valor total dos itens do carrinho de compras.

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ const cartItems = document.querySelector('.cart__items');
 const totalPrice = document.createElement('h3');
 const cart = document.querySelector('.cart');
 const emptyCartBtn = document.querySelector('.empty-cart');
+
 totalPrice.className = 'total-price';
-totalPrice.innerText = 'Subtotal: R$ 0';
+totalPrice.innerText = '0';
 totalPrice.style.textAlign = 'center';
 totalPrice.style.marginBottom = '20px';
+
 cart.insertBefore(totalPrice, emptyCartBtn);
+
+// const addLoadingScreen = () => {
+//   const loading = document.createElement('h2');
+//   loading.innerText = 'Loading...';
+//   loading.style.background = 'black';
+//   loading.style.color = 'white';
+  
+// };
+
+const localStorageChecker = () => {
+  if (localStorage.length > 0) {
+    const localStorageCost = localStorage.getItem('cartCost').split('$');
+    return Number(localStorageCost[1]);
+  } return 0;
+};
+
+let cost = localStorageChecker();
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -39,10 +58,29 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const priceUp = (price) => {
+  cost += price;
+  return cost;
+};
+
+const priceDown = (price) => {
+  cost -= price;
+  return cost;
+};
+
+const saveCost = () => {
+  localStorage.setItem('cartCost', totalPrice.innerHTML);
+};
+
 function cartItemClickListener({ target }) {
   if (target.classList.contains('cart__item')) {
     cartItems.removeChild(target);
+    const splitted = target.innerText.split('$');
+    const itemPrice = Number(splitted[1]);
+    const newPrice = priceDown(itemPrice);
+    totalPrice.innerText = newPrice;
     saveCartItems(cartItems);
+    saveCost();
   }
 }
 
@@ -63,16 +101,15 @@ const itens = async () => {
   });
 };
 
-const priceChanger = (price) => {
- 
-};
-
 const cartClear = () => {
  const cartItem = document.querySelectorAll('.cart__item');
  cartItem.forEach((li) => {
   cartItems.removeChild(li);
  });
- totalPrice.innerText = 'Subtotal: R$ 0';
+ localStorage.removeItem('cartList');
+ cost = 0;
+ totalPrice.innerText = cost;
+ saveCost();
 };
 
 emptyCartBtn.addEventListener('click', cartClear);
@@ -83,13 +120,18 @@ sectionItems.addEventListener('click', async ({ target }) => {
     const fisrtChildText = getSkuFromProductItem(parent);
     const item = await fetchItem(fisrtChildText);
     const { id, title, price } = item;
-    const cost = priceChanger(price);
-    totalPrice.innerText = `Subtotal: R$ ${cost}`;
+    const totalCost = priceUp(price);
+    totalPrice.innerText = totalCost;
     cartItems.appendChild(createCartItemElement({ sku: id, name: title, salePrice: price }));
     saveCartItems(cartItems);
+    saveCost();
   }
 });
 
+const getSavedCost = () => {
+  const totalCost = localStorage.getItem('cartCost');
+  totalPrice.innerHTML = totalCost; 
+};
 const getRidOf = () => {
   getSavedCartItems();
   const li = document.querySelectorAll('li.cart__item');
@@ -101,4 +143,5 @@ const getRidOf = () => {
 window.onload = async () => {
   await itens();
   getRidOf();
+  getSavedCost();
  };
