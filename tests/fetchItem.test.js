8981bd802377cc30294dc710329f643def840237
RabeloGuedes <krepscredian@gmail.com>
2022-07-22 16:50:13 -0300

    fix: Remove url desnecessária do arquivo de teste fetchItem.test.js

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -21,7 +21,6 @@ describe('2 - Teste a função fecthItem', () => {
   });
   test('Teste se o retorno da função fetchItem com o argumento do item "MLB1615760527" é uma estrutura de dados igual ao objeto item que já está importado no arquivo.', async () => {
     expect.assertions(1);
-    const url = "https://api.mercadolibre.com/items/MLB1615760527";
     const response = await fetchItem('MLB1615760527');
     expect(response).toEqual(item);
   });
